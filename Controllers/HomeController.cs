9c9f13737121f6495a727e4278f8dbae81bebff0
ken.ngyn <ken.ngyn@outlook.com>
2021-03-18 01:56:05 +0800

    First commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SitecoreSEOAnalyzer.Models;
+
+namespace SitecoreSEOAnalyzer.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index(Home model, string sortOrder)
+        {
+            if (!string.IsNullOrEmpty(model.Text))
+            {
+                if (ModelState.IsValid)
+                {
+                    try
+                    {
+                        HttpClient client = new HttpClient();
+                        var response = await client.GetAsync(model.Text);
+                        var pageContents = await response.Content.ReadAsStringAsync();
+                        HtmlDocument pageDocument = new HtmlDocument();
+                        pageDocument.LoadHtml(pageContents);
+
+
+                        // page occurrence
+                        var getPage = pageDocument.DocumentNode.InnerHtml;
+                        var pageOccurrence = CalculateStopwordOccurrence(getPage);
+                        model.PageOccurrences = pageOccurrence;
+
+                        // meta occurrence
+                        var meta = new StringBuilder();
+                        var getMeta = pageDocument.DocumentNode.SelectNodes("//meta");
+                        foreach (var node in getMeta)
+                        {
+                            meta.Append(node.GetAttributeValue("content", string.Empty));
+                        }
+                        var metaOccurrence = CalculateStopwordOccurrence(meta.ToString());
+                        model.MetaOccurrences = metaOccurrence;
+
+                        // external links occurrence
+                        var externalLinks = new StringBuilder();
+                        var getExternalLinks = pageDocument.DocumentNode.SelectNodes("//a[@href]");
+                        foreach (var node in getExternalLinks)
+                        {
+                            externalLinks.Append(node.GetAttributeValue("href", string.Empty)).Append("|");
+                        }
+                        var linkOccurrence = CalculateLinkOccurrence(externalLinks.ToString());
+                        model.ExternalLinks = linkOccurrence;
+                    }
+                    catch (Exception e)
+                    {
+                        ModelState.AddModelError("Text", "Please try again with a different URL");
+                    }
+                }
+            }
+
+            return View(model);
+        }
+
+        private Dictionary<string, int> CalculateStopwordOccurrence(string text)
+        {
+            var stopwords = new Stopword().Stopwords;
+
+            Dictionary<string, int> occurrence = new Dictionary<string, int>();
+            char[] delimiters = { ' ', '.', ',', ';', ':', '?', '\n', '\r' };
+            string[] words = text.Split(delimiters);
+
+            foreach (string word in words)
+            {
+                string w = word.Trim().ToLower();
+                if (stopwords.ContainsKey(w))
+                {
+                    if (!occurrence.ContainsKey(w))
+                    {
+                        occurrence.Add(w, 1);
+                    }
+                    else
+                    {
+                        occurrence[w] += 1;
+                    }
+                }
+            }
+
+            return occurrence;
+        }
+
+        private Dictionary<string, int> CalculateLinkOccurrence(string text)
+        {
+            Dictionary<string, int> occurrence = new Dictionary<string, int>();
+            char[] delimiters = { '|' };
+            string[] words = text.Split(delimiters);
+
+            foreach (var word in words)
+            {
+                string w = word.Trim().ToLower();
+                if (w.Contains("http"))
+                {
+                    if (!occurrence.ContainsKey(w))
+                    {
+                        occurrence.Add(w, 1);
+                    }
+                    else
+                    {
+                        occurrence[w] += 1;
+                    }
+                }
+            }
+
+            return occurrence;
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Home.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentValidation;
+
+namespace SitecoreSEOAnalyzer.Models
+{
+    public class Home
+    {
+        public string Text { get; set; }
+
+        public Dictionary<string, int> PageOccurrences { get; set; } = new Dictionary<string, int>();
+
+        public Dictionary<string, int> MetaOccurrences { get; set; } = new Dictionary<string, int>();
+
+        public Dictionary<string, int> ExternalLinks { get; set; } = new Dictionary<string, int>();
+    }
+
+    public class HomeValidator : AbstractValidator<Home>
+    {
+        public HomeValidator()
+        {
+            RuleFor(x => x.Text)
+                .Must(uri => Uri.TryCreate(uri, UriKind.Absolute, out _)).When(x => !string.IsNullOrEmpty(x.Text))
+                .WithMessage("Please enter a valid URL");
+        }
+    }
+}
